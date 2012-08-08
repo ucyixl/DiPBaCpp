8b409f7c2026218a55261769c635c497eed8d401
Silvia Liverani <liveranis@gmail.com>
2012-08-08 14:41:14 +0100

    Bug and minor changes

--- rfiles/DiPBaC/src/include/postProcess.h
+++ rfiles/DiPBaC/src/include/postProcess.h
@@ -3,6 +3,20 @@
 
 #include <Rcpp.h>
 
+// Note that the functions Gamma and LogGamma are mutually dependent.
+double LogGamma(double);
+double Gamma(double);
+
 RcppExport SEXP calcDisSimMat(SEXP fileName, SEXP nSweeps, SEXP nBurn, SEXP nFilter,SEXP nSubjects,SEXP nPredictSubjects);
+
+RcppExport SEXP pYGivenZW(SEXP betaIn,SEXP thetaIn,SEXP zAlloc,SEXP sigmaBeta,
+		SEXP sigmaTheta, SEXP dofTheta, SEXP dofBeta, SEXP nSubjects,
+		SEXP yMat,SEXP betaW,SEXP nFixedEffects,SEXP nNames,SEXP constants,
+		SEXP maxnNames);
+
+RcppExport SEXP GradpYGivenZW(SEXP betaIn,SEXP thetaIn,SEXP zAlloc, SEXP nSubjects,
+		SEXP betaW,SEXP yMat, SEXP nFixedEffects,SEXP nNames,SEXP maxnNames);
+
+RcppExport SEXP pZpX(SEXP nClusters, SEXP nCategories,SEXP aPhi, SEXP n,SEXP nCovariates,SEXP zAlloc, SEXP xMat, SEXP nNames,SEXP alpha);
 
 #endif
